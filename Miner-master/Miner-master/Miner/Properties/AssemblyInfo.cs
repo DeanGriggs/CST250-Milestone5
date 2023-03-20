1056070402a40e6a9d9308bbe1283880982ab3ef
DeanGriggs <117603385+DeanGriggs@users.noreply.github.com>
2023-03-19 17:12:58 -0700

    Add files via upload

---
+++ Miner-master/Miner-master/Miner/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("")]
+
+
+[assembly: AssemblyVersion("")]
+[assembly: AssemblyFileVersion("")]
